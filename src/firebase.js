0fc22b5c5ba258144c82e4ee8479f8507418fada
EgilVDW <56960281+EgilVDW@users.noreply.github.com>
2026-03-23 10:10:21 +0100

    init

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6Pt4KkOp3dfCP0n0cNtXIH0BBGTe3W18",
+  authDomain: "pentia-1.firebaseapp.com",
+  projectId: "pentia-1",
+  storageBucket: "pentia-1.firebasestorage.app",
+  messagingSenderId: "106566492564",
+  appId: "1:106566492564:web:e9877618e195f17ee4ef81",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted } from "vue";
+import { db } from "@/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+async function getData() {
+  const querySnapshot = await getDocs(collection(db, "test"));
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, doc.data());
+  });
+}
+
+onMounted(() => {
+  getData();
+});
+</script>
+<template>
+  <h1>Hej</h1>
+  <h2>Hej</h2>
+  <p>Hej</p>
+  <button>Hej</button>
+  <ul>
+    <li>Hej</li>
+    <li>Hej</li>
+    <li>Hej</li>
+  </ul>
+</template>
