552a1f01c0ac11185a8c10c519c0ee91839d13d5
Carina <54623517+chvessel@users.noreply.github.com>
2026-04-09 22:54:15 +0200

    add searchInput

--- src/components/base/form/SearchInput.vue
+++ src/components/base/form/SearchInput.vue
@@ -1,9 +1,22 @@
 <script setup>
+import { ref, watch } from 'vue';
 
+const searchText = ref('');
+const emit = defineEmits(['search-change']);
+
+watch(searchText, (newValue) => {
+    emit('search-change', newValue);
+});
 </script>
 
 <template>
-    
+    <div class="search-bar">
+        <!-- magnifying glass icon here -->
+         <input 
+         type="text"
+         v-model="searchText"
+         placeholder="Søg">
+    </div>
 </template>
 
 <style scoped>
