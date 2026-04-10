d87192023e674b55d5718853d8cc3b7ca48b3d45
Carina <54623517+chvessel@users.noreply.github.com>
2026-04-10 11:18:14 +0200

    add LoginInput

---
+++ src/components/LoginInput.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+const model = defineModel();
+const props = defineProps({
+    placeholder: String
+});
+</script>
+
+<template>
+<div class="login-field">
+    <input 
+        type="text"
+        v-model="model"
+        :placeholder="placeholder"
+        class="login-field__input"
+    />
+</div>
+</template>
+
+<style scoped>
+
+</style>
