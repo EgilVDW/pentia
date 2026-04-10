4209f53fdb72dbba7a03afc3c7d075264e18e6c5
Carina <54623517+chvessel@users.noreply.github.com>
2026-04-10 13:09:40 +0200

    add LoginPassword

--- src/components/LoginPassword.vue
+++ src/components/LoginPassword.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { ref } from 'vue';
 
+const model = defineModel();
+const props = defineProps({
+    placeholder: String
+});
+
+const isVisible = ref(false);
 </script>
 
 <template>
+    <div class="login-password">
+        <input 
+            :type="isVisible ? 'text' : 'password'" 
+            v-model="model" 
+            :placeholder="placeholder"
+            class="login-password__input" />
 
+        <button 
+            type="button" 
+            @click="isVisible = !isVisible" 
+            class="login-password__toggle">
+            
+            <!--i :class="isVisible = ? ('icon here') : '(icon here)'"i-->
+        </button>
+    </div>
 </template>
 
 <style scoped>
