3570c003d19ad219917ec8c8de47c2dbbcd26d0e
Egil W <egil.wendsjo@gmail.com>
2026-04-10 15:15:50 +0200

    add scss structure and variables

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/base/reset" as *;
+          @use "@/assets/styles/abstracts/colors" as *;
+          @use "@/assets/styles/abstracts/typography" as *;
+          @use "@/assets/styles/abstracts/variables" as *;
+          @use "@/assets/styles/base/global" as *;`,
+      },
+    },
+  },
+});
