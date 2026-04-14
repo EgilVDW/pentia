17a44176d7a4b847ff9f93ec90a56afd4502c885
Egil W <egil.wendsjo@gmail.com>
2026-04-14 11:42:43 +0200

    update Icon

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="svgContent" v-html="svgContent"></div>
+  <div v-if="svgContent" v-html="svgContent" class="icon"></div>
 </template>
 <script setup>
 import { computed } from "vue";
@@ -16,8 +16,12 @@ const icons = import.meta.glob("../assets/icons/*.svg", {
 
 const svgContent = computed(() => {
   const key = `../assets/icons/${props.name}.svg`;
-  const raw = icons[key] || null;
-  if (!raw) return null;
   return icons[key] || null;
 });
 </script>
+<style lang="scss">
+.icon svg {
+  width: 100%;
+  height: 100%;
+}
+</style>
