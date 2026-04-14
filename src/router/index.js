9c021e093b531d151997d2515fc7a268603bb6bd
Egil W <egil.wendsjo@gmail.com>
2026-04-14 11:50:10 +0200

    add ProfileView

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import PlaceholderView from "@/views/PlaceholderView.vue";
-
+import ProfileView from "@/views/ProfileView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +9,19 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView
+      component: HomeView,
     },
     {
       path: "/placeholder",
       name: "placeholder",
-      component: PlaceholderView
-    }
-  ]
+      component: PlaceholderView,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfileView,
+    },
+  ],
 });
 
 export default router;
