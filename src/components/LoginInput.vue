d160e0ba24ccc95e9057cb5eaea6c78c5d3784b0
Carina <54623517+chvessel@users.noreply.github.com>
2026-04-14 14:20:54 +0200

    edit and style LoginInput.vue

--- src/components/LoginInput.vue
+++ src/components/LoginInput.vue
@@ -1,22 +1,60 @@
 <script setup>
-
 const model = defineModel();
 const props = defineProps({
-    placeholder: String
+    placeholder: {
+        type: String,
+        default: 'Brugernavn'
+    },
+    autocomplete: {
+        type: String, 
+        default: 'Brugernavn'
+    },
+    name:{
+        type: String,
+        default: 'Brugernavn'
+    }
 });
 </script>
 
 <template>
-<div class="login-field">
-    <input 
-        type="text"
-        v-model="model"
-        :placeholder="placeholder"
-        class="login-field__input"
-    />
-</div>
+    <div class="login-input">
+        <input 
+            type="text"
+            v-model="model"
+            :placeholder="placeholder"
+            :autocomplete="autocomplete"
+            :name="name"
+            class="login-input__field"
+        />
+    </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.login-input {
+    width: 100%;
+    margin-bottom: 10px; 
+    background-color: $color-surface;
+    border-radius: $border-radius-large;
 
+    &__field {
+        width: 100%;
+        padding: 8px 25px;
+        border: none;
+        border-radius: $border-radius-large; 
+        font-size: 16px; 
+        font-family: "Inter";
+        transition: border-color 0.2s ease;
+        color: $color-background;
+
+        &::placeholder {
+            color: $color-placeholder;
+        }
+
+        &:focus {
+            outline: none;
+            border-color: #1a3d4a; 
+            color: $color-foreground;
+        }
+    }
+}
 </style>
