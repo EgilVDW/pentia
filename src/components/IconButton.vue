7811bca8bccca4775225364f53dcd5ceb26e7a93
Egil W <egil.wendsjo@gmail.com>
2026-04-14 11:47:03 +0200

    add IconButton

---
+++ src/components/IconButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import BaseButton from "@/components/BaseButton.vue";
+import IconLabel from "@/components/IconLabel.vue";
+
+defineProps({
+  icon: String,
+});
+</script>
+<template>
+  <BaseButton v-bind="$attrs" class="icon-button">
+    <IconLabel :icon="icon">
+      <slot />
+    </IconLabel>
+  </BaseButton>
+</template>
+<style scoped lang="scss">
+.icon-button {
+  font-size: $font-size-body;
+  line-height: $line-height-body;
+}
+</style>
