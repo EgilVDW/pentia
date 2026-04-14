f8345a6a8860bca065b641daadd33cdcf53f3298
Egil W <egil.wendsjo@gmail.com>
2026-04-14 11:46:00 +0200

    add BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed } from "vue";
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  href: String,
+  external: Boolean,
+  type: String,
+});
+
+const component = computed(() => {
+  if (!props.href) return "button";
+  if (!props.external) return RouterLink;
+  return "a";
+});
+
+const componentAttrs = computed(() => {
+  if (!props.href) {
+    return { type: props.type || "button" };
+  }
+
+  if (!props.external) {
+    return { to: props.href };
+  }
+
+  return {
+    href: props.href,
+    rel: "noopener noreferrer",
+  };
+});
+</script>
+<template>
+  <component :is="component" v-bind="componentAttrs" class="base-button">
+    <slot />
+  </component>
+</template>
+<style scoped lang="scss">
+.base-button {
+  display: flex;
+  align-items: center;
+  padding: 0 0.625rem;
+  width: 100%;
+  height: 2.375rem;
+  border-radius: $border-radius-large;
+  background-color: $color-surface;
+  box-shadow: $drop-shadow;
+}
+</style>
