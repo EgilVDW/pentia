432c29ac8a08f65926070773e7d97d6f23fadfdf
Egil W <egil.wendsjo@gmail.com>
2026-04-14 11:46:46 +0200

    add IconLabel

---
+++ src/components/IconLabel.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Icon from "@/components/Icon.vue";
+
+defineProps({
+  icon: String,
+});
+</script>
+<template>
+  <span class="icon-label">
+    <Icon v-if="icon" :name="icon" class="icon-label__icon" />
+    <slot />
+  </span>
+</template>
+<style scoped lang="scss">
+.icon-label {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.3125rem;
+
+  &__icon {
+    flex-shrink: 0;
+    width: 1rem;
+    height: 1rem;
+    color: $color-primary;
+  }
+}
+</style>
