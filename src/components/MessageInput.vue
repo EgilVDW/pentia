7ebe0cbdd4237631ffa6a0df388f7d272c6e62dc
EgilVDW <56960281+EgilVDW@users.noreply.github.com>
2026-04-21 12:45:43 +0200

    update MessageInput

--- src/components/MessageInput.vue
+++ src/components/MessageInput.vue
@@ -8,26 +8,57 @@ const props = defineProps({
 
 const emit = defineEmits(["update:modelValue", "send"]);
 
-function onInput(e) {
+const onInput = (e) => {
   emit("update:modelValue", e.target.value);
-}
+};
 
-function handleSend() {
+const handleSend = () => {
   const text = props.modelValue.trim();
   if (!text) return;
 
   emit("send", text);
   emit("update:modelValue", "");
-}
+};
 </script>
 <template>
-  <form @submit.prevent="handleSend">
+  <form class="message-input" @submit.prevent="handleSend">
     <input
       :value="modelValue"
-      @input="onInput"
       type="text"
       placeholder="Skriv besked"
+      class="message-input__field"
+      @input="onInput"
     />
-    <button type="submit" :disabled="!modelValue.trim()">Send</button>
+    <button
+      type="submit"
+      :disabled="!modelValue.trim()"
+      class="message-input__button"
+    >
+      Send
+    </button>
   </form>
 </template>
+<style scoped lang="scss">
+.message-input {
+  display: flex;
+  border-radius: $border-radius-small;
+  background-color: $color-surface;
+
+  &__field {
+    flex-grow: 1;
+    padding: 0.5rem;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+  }
+
+  &__button {
+    margin: 0.5rem;
+    padding: 0.1875rem 1.75rem;
+    border-radius: $border-radius-small;
+    background-color: $color-primary;
+    color: $color-surface;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+  }
+}
+</style>
