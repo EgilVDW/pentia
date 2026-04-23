2a389eae0ff930e1bee271f3ffdb9e5eefa42643
Carina <54623517+chvessel@users.noreply.github.com>
2026-04-23 21:02:03 +0200

    add styling and changes

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,28 +1,25 @@
 <script setup>
 import ConstructionGallery from "@/components/ConstructionGallery.vue";
 
-
 </script>
 
 <template>
-  <div class="gallery-view">
-    <h1><strong>Byggeplads billeder</strong></h1>
+  <main class="gallery-view">
+    <h1>Byggeplads billeder</h1>
     <ConstructionGallery />
-  </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 .gallery-view {
-  padding: 40px 20px;
-  background-color: $color-secondary;
-  min-height: 100vh;
   box-sizing: border-box;
 
   h1{
     color: $color-foreground;
     font-size: $font-size-headline-1;
     margin-bottom: 10px;
-    padding-left: 5px;
+    font-weight: 700;
+    padding-left: 4px;
     text-transform: none;
   }
 }
