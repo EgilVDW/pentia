e7433e4f800508a6d4203fdd6aa1f34c3c81d003
Egil W <egil.wendsjo@gmail.com>
2026-04-26 23:31:39 +0200

    update ProfileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Heading from "@/components/Heading.vue";
 import ProfileCard from "@/components/ProfileCard.vue";
 import Paragraph from "@/components/Paragraph.vue";
 import DetailsCard from "@/components/DetailsCard.vue";
@@ -36,22 +37,73 @@ const notifications = [
 ];
 </script>
 <template>
-  <ProfileCard
-    avatar="/images/images_bygherre/Familien_milton_profile_picture.png"
-    name="Familien Milton"
-    email="Familienmilton@email.dk"
-  />
-  <Paragraph
-    >Følg dit byggeprojekt og modtag opdateringer fra din byggeleder</Paragraph
-  >
-  <DetailsCard :items="details" />
-  <ContactCard
-    avatar="/images/images_bygherre/Kim_profile_picture.png"
-    role="Byggeleder"
-    name="Kim Agerbæk"
-    phoneNumber="+45 1234 5678"
-  />
-  <NotificationList :items="notifications" />
-  <IconButton icon="Indstillinger">Indstillinger</IconButton>
-  <CenterButton>Log ud</CenterButton>
+  <main class="profile-view">
+    <Heading tag="h1" size="large">Profil</Heading>
+    <ProfileCard
+      avatar="/images/images_bygherre/Familien_milton_profile_picture.png"
+      name="Familien Milton"
+      email="Familienmilton@email.dk"
+    />
+    <div class="profile-view__group">
+      <Paragraph
+        >Følg dit byggeprojekt og modtag opdateringer fra din
+        byggeleder</Paragraph
+      >
+      <DetailsCard :items="details" />
+      <ContactCard
+        avatar="/images/images_bygherre/Kim_profile_picture.png"
+        role="Byggeleder"
+        name="Kim Agerbæk"
+        phoneNumber="+45 1234 5678"
+      />
+    </div>
+    <div class="profile-view__group">
+      <Heading tag="h2" size="medium">Notifikationer</Heading>
+      <NotificationList :items="notifications" />
+    </div>
+    <div class="profile-view__button-grid">
+      <div class="profile-view__group">
+        <Heading tag="h2" size="medium">Min konto</Heading>
+        <ul class="profile-view__group">
+          <li>
+            <IconButton icon="Adgangskode">Skift adgangskode</IconButton>
+          </li>
+          <li>
+            <IconButton icon="Indstillinger">Indstillinger</IconButton>
+          </li>
+        </ul>
+      </div>
+      <div class="profile-view__group">
+        <Heading tag="h2" size="medium">Support</Heading>
+        <ul class="profile-view__group">
+          <li>
+            <IconButton icon="Opkald">Kontakt Milton Huse</IconButton>
+          </li>
+          <li>
+            <IconButton icon="Hjaelp">Hjælp til app</IconButton>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <CenterButton>Log ud</CenterButton>
+  </main>
 </template>
+<style scoped lang="scss">
+.profile-view {
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+
+  &__group {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  &__button-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+  }
+}
+</style>
