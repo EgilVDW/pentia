609d06ca7a8b23a40923eebe97b35221df944fa8
Egil W <egil.wendsjo@gmail.com>
2026-05-15 13:32:26 +0200

    add contact store

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <script setup>
-import { onMounted } from "vue"
-import { useAuthStore } from "@/stores/auth"
-import { useProjectStore } from "@/stores/project"
-import { useMaterialsStore } from "@/stores/materials"
-import { useDocumentCategoriesStore } from "@/stores/documentCategories"
+import { onMounted } from "vue";
+import { useAuthStore } from "@/stores/auth";
+import { useProjectStore } from "@/stores/project";
+import { useMaterialsStore } from "@/stores/materials";
+import { useDocumentCategoriesStore } from "@/stores/documentCategories";
+import { useContactStore } from "@/stores/contact";
 
-const authStore = useAuthStore()
-const projectStore = useProjectStore()
-const materialsStore = useMaterialsStore()
-const categoriesStore = useDocumentCategoriesStore()
+const authStore = useAuthStore();
+const projectStore = useProjectStore();
+const materialsStore = useMaterialsStore();
+const categoriesStore = useDocumentCategoriesStore();
+const contactStore = useContactStore();
 
 onMounted(() => {
   // Lytte efter om brugeren er logget ind
-  authStore.initAuth()
+  authStore.initAuth();
 
   // Henter project inkl. subcollections
-  projectStore.fetchProject()
+  projectStore.fetchProject();
 
   // Henter data
-  materialsStore.fetchMaterials()
-  categoriesStore.fetchCategories()
-})
+  materialsStore.fetchMaterials();
+  categoriesStore.fetchCategories();
+});
 </script>
 
 <template>
