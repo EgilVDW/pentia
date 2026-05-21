620383cbfb294172183a9c07b6b54265ee20030c
Benjamin Vid Gard <bbvgard@gmail.com>
2026-05-21 13:58:57 +0200

    Start documentation of difficulties and solutions

---
+++ difficultiesSolutions/difficulties.js
@@ -0,0 +1,16 @@
+/**
+ * Difficulties & Solutions
+ *
+ * Here we will go a little in depth about challenges we ran into and how we overcame them.
+ *
+ * @difficulties
+ *
+ * @design
+ * Throughout the process, there has been many discussions about the design and how we best implement each feature.
+ * The design itself was not so much the problem, as it was being able to identify possible components.
+ *
+ *
+ *
+ * @category guides
+ * @name Difficulties
+ */
